c274687d7bd3e3f175ebc3b1d45813bb6d81ac4b
kelvinleandro <kelvinleandro5@gmail.com>
2024-03-11 21:12:31 -0300

    feat: added FetchingError message component

---
+++ src/components/UI/FetchingError/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components/native";
+import Constants from "expo-constants";
+import { Dimensions, Image } from "react-native";
+
+import COLORS from "@/constants/colors";
+
+const STATUS_BAR_HEIGHT = Constants.statusBarHeight;
+
+const FetchingError = () => {
+  return (
+    <Container>
+      <Title>oops!!</Title>
+      <Subtitle>it was not possible to fetch the data</Subtitle>
+      <Image source={require('@/assets/man.png')} resizeMode="contain" style={{
+        width: Dimensions.get("window").width * 0.7,
+        height: Dimensions.get("window").width * 0.7
+      }} />
+    </Container>
+  );
+};
+
+export default FetchingError;
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 38px;
+`;
+
+const Title = styled.Text`
+  font-size: 48px;
+  text-transform: uppercase;
+  color: ${COLORS.secondary};
+  font-weight: 800;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 22px;
+  text-transform: uppercase;
+  color: ${COLORS.text};
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 18px;
+`;
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,6 @@
 import { ScrollView, Text, View } from "react-native";
 import Constants from "expo-constants";
+
 import { HorizontalList } from "@/components/UI/HorizontalList";
 import useApi from "@/hooks/useApi";
 import {
@@ -8,36 +9,42 @@ import {
   fetchUpcomingMovies,
 } from "@/api/helper";
 
+import FetchingError from "@/components/UI/FetchingError";
+
 const HomeScreen = () => {
-  const { data: trendingMovies } = useApi(fetchTrendingMovies);
-  const { data: upcomingMovies } = useApi(fetchUpcomingMovies);
-  const { data: popularMovies } = useApi(fetchPopularMovies);
+  // const { data: trendingMovies } = useApi(fetchTrendingMovies);
+  // const { data: upcomingMovies } = useApi(fetchUpcomingMovies);
+  // const { data: popularMovies } = useApi(fetchPopularMovies);
 
-  return (
-    <ScrollView
-      style={{
-        flex: 1,
-        paddingTop: Constants.statusBarHeight,
-        paddingHorizontal: 8,
-      }}
-    >
-      <Text style={{color: "white"}} >{trendingMovies?.length}</Text>
-      <HorizontalList.Container>
-        <HorizontalList.Label>Trending</HorizontalList.Label>
-        <HorizontalList.List data={trendingMovies} />
-      </HorizontalList.Container>
+  if (true) {
+    return (<FetchingError />);
+  }
 
-      <HorizontalList.Container>
-        <HorizontalList.Label>Upcoming</HorizontalList.Label>
-        <HorizontalList.List data={upcomingMovies} />
-      </HorizontalList.Container>
+  // return (
+  //   <ScrollView
+  //     style={{
+  //       flex: 1,
+  //       paddingTop: Constants.statusBarHeight,
+  //       paddingHorizontal: 8,
+  //     }}
+  //   >
+  //     <Text style={{ color: "white" }}>{trendingMovies?.length}</Text>
+  //     <HorizontalList.Container>
+  //       <HorizontalList.Label>Trending</HorizontalList.Label>
+  //       <HorizontalList.List data={trendingMovies} />
+  //     </HorizontalList.Container>
 
-      <HorizontalList.Container>
-        <HorizontalList.Label>Popular</HorizontalList.Label>
-        <HorizontalList.List data={popularMovies} />
-      </HorizontalList.Container>
-    </ScrollView>
-  );
+  //     <HorizontalList.Container>
+  //       <HorizontalList.Label>Upcoming</HorizontalList.Label>
+  //       <HorizontalList.List data={upcomingMovies} />
+  //     </HorizontalList.Container>
+
+  //     <HorizontalList.Container>
+  //       <HorizontalList.Label>Popular</HorizontalList.Label>
+  //       <HorizontalList.List data={popularMovies} />
+  //     </HorizontalList.Container>
+  //   </ScrollView>
+  // );
 };
 
 export default HomeScreen;
